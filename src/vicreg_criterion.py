2b18e98c72fa87a71acc7aef2c3645084018e521
sahardastani <sahar.dastani4776@gmail.com>
2023-04-26 08:39:53 -0700

    add vicregal

--- src/vicreg_criterion.py
+++ src/vicreg_criterion.py
@@ -1,19 +1,23 @@
+# Source: https://docs.lightly.ai/self-supervised-learning/examples/vicreg.html
+# Put your images or videos in the data folder:
+
 import torch
 import torchvision
 from torch import nn
-from torchvision import transforms
-from torch.utils.data import DataLoader, Dataset
-from PIL import Image
 
 from lightly.data import LightlyDataset
 from lightly.data.multi_view_collate import MultiViewCollate
+
+## The projection head is the same as the Barlow Twins one
 from lightly.loss import VICRegLoss
+
+## The projection head is the same as the Barlow Twins one
 from lightly.loss.vicreg_loss import VICRegLoss
 from lightly.models.modules import BarlowTwinsProjectionHead
 from lightly.transforms.vicreg_transform import VICRegTransform
 
-from datasets.ucf101 import DoubleTransformBatchDataset
 from models.vicreg import VICReg
+from __init__ import top_dir, data_dir, configs_dir
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -22,34 +26,28 @@ backbone = nn.Sequential(*list(resnet.children())[:-1])
 model = VICReg(backbone)
 model.to(device)
 
+transform = VICRegTransform(input_size=32)
+# create a dataset from a folder containing images or videos:
+dataset = LightlyDataset(data_dir(), transform=transform)
+
+collate_fn = MultiViewCollate()
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    collate_fn=collate_fn,
+    shuffle=True,
+    drop_last=True,
+    num_workers=1,
+)
+
 criterion = VICRegLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.06)
-
-# Define the transformations you want to apply
-transform1 = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(224),
-    transforms.ToTensor(),
-])
-
-transform2 = transforms.Compose([
-    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
-    transforms.RandomRotation(degrees=45),
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
-
-# Create a list of image paths and a dataset that uses the two different transformations
-img_paths = ["flow-frames/frame_0010.png", "flow-frames/frame_0011.png"]
-dataset = DoubleTransformBatchDataset(img_paths, transform1=transform1, transform2=transform2)
-
-# Create a dataloader that uses the dataset, with a batch size of 2
-dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
 
 print("Starting Training")
 for epoch in range(10):
     total_loss = 0
-    for x0, x1 in dataloader:
+    for (x0, x1), _, _ in dataloader:
         x0 = x0.to(device)
         x1 = x1.to(device)
         z0 = model(x0)
