47fcf58228f9552f2bdbe504f8306c6145b9e277
Sahardastani <sahar.dastani4776@gmail.com>
2023-10-11 12:09:15 -0400

    dataloader optimization

--- src/datasets/decoder.py
+++ src/datasets/decoder.py
@@ -305,7 +305,7 @@ def decode(
     sampling_rate,
     num_frames,
     clip_idx=-1,
-    num_clips=10,
+    num_clips=2,
     video_meta=None,
     target_fps=30,
     backend="pyav",
@@ -403,45 +403,45 @@ def decode(
             cur_global, index_global = temporal_sampling(frames, random_idx, max_len - random_idx, num_frames)
             global_samples.append(cur_global)
             global_indexes.append(index_global)
-        local_samples = []
-        local_indexes = []
-        local_width = max_len // 8
-        for _ in range(2):
-            random_idx = random.randint(0, max_len - local_width - 1)
-            cur_local, index_local = temporal_sampling(frames, random_idx, random_idx + local_width, num_frames)
-            local_samples.append(cur_local)
-            local_indexes.append(index_local)
-        frames = [*global_samples, *local_samples]
-        indexes = [*global_indexes, *local_indexes]
+        # local_samples = []
+        # local_indexes = []
+        # local_width = max_len // 8
+        # for _ in range(2):
+        #     random_idx = random.randint(0, max_len - local_width - 1)
+        #     cur_local, index_local = temporal_sampling(frames, random_idx, random_idx + local_width, num_frames)
+        #     local_samples.append(cur_local)
+        #     local_indexes.append(index_local)
+        frames = [*global_samples]#, *local_samples]
+        indexes = [*global_indexes]#, *local_indexes]
     elif temporal_aug:
         max_len = frames.shape[0]
 
         if rand_fr:
             global_1, index_global_1 = temporal_sampling(frames, 0, max_len - 5, 4)
             global_2, index_global_2 = temporal_sampling(frames, 5, max_len, 8)
-            local_samples = []
-            local_indexes = []
-            local_width = max_len // 8
-            num_local_frames = [2, 2, 4, 4, 8, 8, 16, 16]
-            for l_idx in range(8):
-                random_idx = random.randint(0, max_len - local_width - 1)
-                cur_local, index_local = temporal_sampling(frames, random_idx, random_idx + local_width, num_local_frames[l_idx])
-                local_samples.append(cur_local)
-                local_indexes.append(index_local)
+            # local_samples = []
+            # local_indexes = []
+            # local_width = max_len // 8
+            # num_local_frames = [2, 2, 4, 4, 8, 8, 16, 16]
+            # for l_idx in range(8):
+            #     random_idx = random.randint(0, max_len - local_width - 1)
+            #     cur_local, index_local = temporal_sampling(frames, random_idx, random_idx + local_width, num_local_frames[l_idx])
+            #     local_samples.append(cur_local)
+            #     local_indexes.append(index_local)
         else:
             num_global_frames = num_frames
             num_local_frames = num_frames
             global_1 = temporal_sampling(frames, 0, max_len - 5, num_global_frames)
             global_2 = temporal_sampling(frames, 5, max_len, num_global_frames)
-            local_samples = []
-            local_width = max_len // 8
-            for _ in range(8):
-                random_idx = random.randint(0, max_len - local_width - 1)
-                cur_local = temporal_sampling(frames, random_idx, random_idx + local_width, num_local_frames)
-                local_samples.append(cur_local)
+            # local_samples = []
+            # local_width = max_len // 8
+            # for _ in range(8):
+            #     random_idx = random.randint(0, max_len - local_width - 1)
+            #     cur_local = temporal_sampling(frames, random_idx, random_idx + local_width, num_local_frames)
+            #     local_samples.append(cur_local)
 
-        frames = [global_1, global_2, *local_samples]
-        indexes = [index_global_1, index_global_2, *local_indexes]
+        frames = [global_1, global_2]#, *local_samples]
+        indexes = [index_global_1, index_global_2]#, *local_indexes]
 
     else:
         frames, indexes = temporal_sampling(frames, start_idx, end_idx, num_frames)  # frames.shape = (T, H, W, C)
